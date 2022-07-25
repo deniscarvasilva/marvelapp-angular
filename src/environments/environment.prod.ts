f11f6ad673793450d6fac6506bb7273a3878c7b5
Denis Carvalho <deniscarvasilva@gmail.com>
2022-07-25 15:38:58 -0300

    configurações de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://gateway.marvel.com:443/v1/public/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiURL: 'https://gateway.marvel.com:443/v1/public/'
 };
 
+
+export const accessRequest = {
+  ts: '1',
+  public_key: '6eb0a966dabd8c88737f03befe6c4dbf',
+  hash: 'b474dac9518c1c4806850d108e8f4300',
+
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
