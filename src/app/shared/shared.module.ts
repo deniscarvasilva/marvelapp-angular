dcd537d4ef374160ce39a81d670862c6fc71f91f
Denis Carvalho <deniscarvasilva@gmail.com>
2022-07-26 10:32:49 -0300

    estrutura header - search

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
 
 
 @NgModule({
-  declarations: [ HeaderComponent],
+  declarations: [ HeaderComponent, SearchComponent],
   imports: [ CommonModule, RouterModule ],
-  exports: [RouterModule, HeaderComponent]
+  exports: [RouterModule, HeaderComponent, SearchComponent]
 })
 export class SharedModule { }
