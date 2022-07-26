0534ffa6cdff1c3b0493388f18cd702f72a6f988
Denis Carvalho <deniscarvasilva@gmail.com>
2022-07-26 10:41:07 -0300

    criação de model Hero

---
+++ src/app/models/hero.ts
@@ -0,0 +1,60 @@
+export class Hero {
+}
+export class Character {
+    id: number;
+    name: string;
+    description: string;
+    thumbnail: Image;
+    resourceURI: string;
+    comics: Arrival;
+    series: Arrival;
+    stories: Arrival;
+    events: Arrival;
+    urls: URLHero[];
+  
+  
+    constructor(
+      id: number,
+      name: string,
+      description: string,
+      thumbnail: Image,
+      resourceURI: string,
+      comics: Arrival,
+      series: Arrival,
+      stories: Arrival,
+      events: Arrival,
+      urls: URLHero[]
+    ) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+      this.thumbnail = thumbnail;
+      this.resourceURI = resourceURI;
+      this.comics = comics;
+      this.series = series;
+      this.stories = stories;
+      this.events = events;
+      this.urls = urls;
+  
+    }
+  }
+  
+  interface Image {
+    path: string;
+    extension: string;
+  }
+  interface Arrival {
+    available: number;
+    collectionURI: string;
+    items: Items[];
+    returned: number;
+  }
+  interface Items {
+    resourceURI: string;
+    name: string;
+    type?: string;
+  }
+  interface URLHero {
+    type: string;
+    url: string;
+  }
